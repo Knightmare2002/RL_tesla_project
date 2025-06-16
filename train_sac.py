cac34ebffb99ee37d77e6132765ebdb29eb3d5f6
Samuele <samuele1.caruso@gmail.com>
2025-06-16 12:30:06 +0200

    Creation of the project folder

--- train_sac.py
+++ train_sac.py
@@ -1,11 +1,13 @@
 from stable_baselines3 import SAC
 from stable_baselines3.sac.policies import MlpPolicy
-from env.webots_remote_env import WebotsRemoteEnv  # salva la classe sopra in questo file
+from webots_remote_env import WebotsRemoteEnv  # salva la classe sopra in questo file
 
 env = WebotsRemoteEnv()
 
-model = SAC(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=10000)
-
-model.save("webots_sac_model")
-env.close()
+try:
+    model = SAC(MlpPolicy, env, verbose=0, device='cuda')
+    print(f'Using device: {model.device}')
+    model.learn(total_timesteps=2_000_000, progress_bar=True)
+    model.save("webots_sac_model")
+finally:
+    env.close()  # Assicura che venga sempre chiamato anche se il training fallisce
